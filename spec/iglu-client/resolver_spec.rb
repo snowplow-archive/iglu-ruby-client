d7e20bd5d9b6576662fdaa3df49da6aa0bfbf579
Michal Wrobel <sparrovv@gmail.com>
2017-08-09 17:20:21 +0200

    Fix schemas caching issue
    Caching didn't work properly and was keep adding new entries for the
    same schemas.
    
    The underlying problem was with how ruby handles hash key lookups.
    
    https://ruby-doc.org/core-2.2.0/Hash.html#class-Hash-label-Hash+Keys
    
    ```
    Two objects refer to the same hash key when their hash value is
    identical and the two objects are eql? to each other.
    ```
    
    `SchemaKey` and `SchemaVer` were only implementing `==`, but not `eql?` and
    `hash`.
    
    We could fix the problem by adding those methods, but more elegant is
    using Struct class: https://ruby-doc.org/core-2.2.0/Struct.html
    
    Struct gives `==`, `eql?` and `hash` so it can be used as a compound hash key.

--- spec/iglu-client/resolver_spec.rb
+++ spec/iglu-client/resolver_spec.rb
@@ -23,13 +23,13 @@ describe Iglu do
     invalid_config = '{"schema": "iglu:com.snowplowanalytics.iglu/resolver-config/jsonschema/1-0-0", "data": { "cacheSize": null, "repositories": [ { "name": "Iglu Central", "priority": 1, "vendorPrefixes": [ "com.snowplowanalytics" ], "connection": { "http": { "uri": "http://iglucentral.com" } } } ] } }'
     @json_invalid_config = JSON.parse(invalid_config, {:symbolize_names => true})
   }
-  
+
   it 'correctly parses a standard resolver configuration' do
     resolver = Iglu::Resolver.parse(@json_config)
     expect(resolver.registries.length) == 2
     expect(resolver.registries[1].config.name) == "Iglu Central"
   end
-  
+
   it 'throws an exception on resolver configuration not matching its JSON Schema' do
     expect { Iglu::Resolver.parse(@json_invalid_config) }.to raise_error(JSON::Schema::ValidationError)
   end
@@ -41,13 +41,15 @@ describe Iglu do
     key1 = Iglu::SchemaKey.new("com.snowplowanalytics.snowplow.enrichments", "api_request_enrichment_config", "jsonschema", Iglu::SchemaVer.new(1,0,0))
     key2 = Iglu::SchemaKey.new("com.snowplowanalytics.snowplow", "duplicate", "jsonschema", Iglu::SchemaVer.new(1,0,0))
     key3 = Iglu::SchemaKey.new("com.snowplowanalytics.iglu", "resolver-config", "jsonschema", Iglu::SchemaVer.new(1,0,0))
+    key4 = Iglu::SchemaKey.new("com.snowplowanalytics.iglu", "resolver-config", "jsonschema", Iglu::SchemaVer.new(1,0,0))
 
     resolver.lookup_schema(key1)
     resolver.lookup_schema(key2)
     resolver.lookup_schema(key2)
     resolver.lookup_schema(key3)
+    resolver.lookup_schema(key4)
 
-    expect(resolver.cache.size) === 3
+    expect(resolver.cache.size).to eq(3)
   end
 
   it 'can lookup schema in bootstrap resolver' do
